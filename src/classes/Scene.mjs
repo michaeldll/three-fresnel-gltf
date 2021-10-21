f7bb3706d23ea4b7e27489f661eef5e64b5b2ae2
Michael <ac2031@hotmail.com>
2021-10-21 13:48:20 +0200

    Remove useless param, inverse fresnel factor

--- src/classes/Scene.mjs
+++ src/classes/Scene.mjs
@@ -44,22 +44,22 @@ class Scene {
 			uniform float uFresnelWidth;
 
 			void main(){
-					// // Direction du vertex par rapport a la position de la camera
+					// Direction du vertex par rapport a la position de la camera
 					vec3 viewDirection = normalize(cameraPosition - vPosition);
 
-					// // Angle entre deux vecteurs avec un produit scalaire :
-					// // direction d'en haut et la normal
+					// Angle entre deux vecteurs avec un produit scalaire :
+					// direction de la vue et la normal
 					float fresnelFactor = dot(viewDirection, vNormal);
 
-					// // Inverser l'angle
+					// Inverser l'angle
 					float inverseFresnelFactor = clamp(1. - fresnelFactor, 0., 1.);
 
-					// // Shaping function
-					inverseFresnelFactor = step(uFresnelWidth, inverseFresnelFactor);
+					// Shaping function
+					inverseFresnelFactor = step(uFresnelWidth, fresnelFactor);
 
-					vec3 color = mix(uBaseColor, uFresnelColor, inverseFresnelFactor);
+					vec3 color = mix(uBaseColor, uFresnelColor, fresnelFactor);
 
-					gl_FragColor = vec4(color, inverseFresnelFactor);
+					gl_FragColor = vec4(color, fresnelFactor);
 			}
 			`,
 			uniforms: {
@@ -83,8 +83,8 @@ class Scene {
 		});
 
 		// Simple Sphere
-		this.geometry = new THREE.SphereGeometry(0.2, 30, 30);
-		this.mesh = new THREE.Mesh(this.geometry, this.shaderMaterial);
+		// this.geometry = new THREE.SphereGeometry(0.2, 30, 30);
+		// this.mesh = new THREE.Mesh(this.geometry, this.shaderMaterial);
 		// this.scene.add(this.mesh);
 
 		// Light
@@ -110,21 +110,13 @@ class Scene {
 
 	tweaks = () => {
 		const pane = new Tweakpane();
-		const wInput = pane.addInput(this.params, 'fWidth', { min: 0, max: 1 });
-		wInput.on('change', (e) => {
+		const widthInput = pane.addInput(this.params, 'fWidth', { min: 0, max: 1 });
+		widthInput.on('change', (e) => {
 			this.shaderMaterial.uniforms.uFresnelWidth.value = 1 - e.value;
 		});
-		const fCInput = pane.addInput(this.params, 'fColor');
-		fCInput.on('change', (e) => {
+		const fresnelColorInput = pane.addInput(this.params, 'fColor');
+		fresnelColorInput.on('change', (e) => {
 			this.shaderMaterial.uniforms.uFresnelColor.value = new THREE.Vector3(
-				e.value.r / 255,
-				e.value.g / 255,
-				e.value.b / 255
-			);
-		});
-		const bCInput = pane.addInput(this.params, 'baseColor');
-		bCInput.on('change', (e) => {
-			this.shaderMaterial.uniforms.uBaseColor.value = new THREE.Vector3(
 				e.value.r / 255,
 				e.value.g / 255,
 				e.value.b / 255
